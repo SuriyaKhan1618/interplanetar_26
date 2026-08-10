1736e5142fd884dc8650f2f341e10cfb74934763
Suriya Khan <sfkhan1618@gmail.com>
2026-08-10 18:14:25 +0600

    Optimized .sdf and launchfile for Burger tracking

--- quad_controller_ws/src/quad_sim/launch/sim.launch.py
+++ quad_controller_ws/src/quad_sim/launch/sim.launch.py
@@ -31,12 +31,27 @@ def generate_launch_description():
         output='screen'
     )
 
+    burger_spawn = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turtlebot3_gazebo'),
+                'launch',
+                'spawn_turtlebot3.launch.py'
+            )
+        ),
+        launch_arguments={
+            'x_pose':'-2.0',
+            'y_pose':'0.0'
+        }.items()
+    )
+
     ros_gz_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
             '/quadrotor/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
-            'model/quadrotor/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry'
+            'model/quadrotor/odometry@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+            '/model/quadrotor/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
         ],
         output='screen'
     )
@@ -44,5 +59,6 @@ def generate_launch_description():
     return LaunchDescription([
         gazebo_sim,
         spawn_quadrotor,
+        burger_spawn,
         ros_gz_bridge
     ])
