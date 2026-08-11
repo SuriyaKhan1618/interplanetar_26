68353a28456e5fe806d4ff61d829e92f5354d2d7
Suriya Khan <sfkhan1618@gmail.com>
2026-08-11 15:01:53 +0600

    Parsed JSON waypoints

--- auto_nav_ws/src/navigator/navigator/navigator.py
+++ auto_nav_ws/src/navigator/navigator/navigator.py
@@ -1,16 +1,27 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
 
 import asyncio
 import threading
 import websockets
+import math
+import json
+
+def yaw_to_quaternion(yaw):
+    return(
+        0.0,
+        0.0,
+        math.sin(yaw/2.0),
+        math.cos(yaw/2.0)
+    )
 
 class Navigator(Node):
     def __init__(self):
         super().__init__("navigator")
 
-        self.publisher = self.create_publisher(String, "chatter", 10)
+        self.publisher = self.create_publisher(Path, "/path", 10)
         self.url = "ws://localhost:8765"
 
         self.is_running = True
@@ -31,13 +42,13 @@ class Navigator(Node):
             try:
                 async with websockets.connect(self.url) as socket:
                     while self.is_running and rclpy.ok():
-                        waypoints = await socket.recv()
+                        data = await socket.recv()
 
-                        msg = String()
-                        msg.data = str(waypoints)
-                        self.publisher.publish(msg)
+                        path_msg = self.parse_json(data)
+                        if path_msg:
+                            self.publisher.publish(path_msg)
 
-            except(websockets.exceptions.ConncetionClosedError,
+            except(websockets.exceptions.ConnectionClosedError,
                    ConnectionRefusedError) as e:
                 self.get_logger().warn(f"Connection lost: {e}")
                 await asyncio.sleep(2.0)
@@ -45,6 +56,37 @@ class Navigator(Node):
             except Exception as e:
                 self.get_logger().warn(f"Error: {e}")
                 await asyncio.sleep(2.0)
+
+    def parse_json(self, raw_json):
+        try:
+            raw_data = json.loads(raw_json)
+            waypoints = raw_data.get("waypoints", [])
+
+            path_msg = Path()
+            path_msg.header.stamp = self.get_clock().now().to_msg()
+            path_msg.header.frame_id = 'odom'
+
+            for waypoint in waypoints:
+                pose_stamped = PoseStamped()
+                pose_stamped.header = path_msg.header
+
+                pose_stamped.pose.position.x = float(waypoint.get("x", 0.0))
+                pose_stamped.pose.position.y = float(waypoint.get("y", 0.0))
+                pose_stamped.pose.position.z = 0.0
+
+                qx, qy, qz, qw = yaw_to_quaternion(waypoint.get("yaw", 0.0))
+                pose_stamped.pose.orientation.x = qx
+                pose_stamped.pose.orientation.y = qy
+                pose_stamped.pose.orientation.z = qz
+                pose_stamped.pose.orientation.w = qw
+
+                path_msg.poses.append(pose_stamped)
+
+            return path_msg
+
+        except json.JSONDecodeError as e:
+            self.get_logger().warn(f"Error: {e}")
+            return None
 
     def destroy_node(self):
         self.is_running = False
